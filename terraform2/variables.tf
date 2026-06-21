a43b80021c8dae9b2f96700268cb0b604c9c9508
chetan8982 <chetant1902@gmail.com>
2026-06-21 05:29:24 +0000

    update file

---
+++ terraform2/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Cloud Run service name"
+  type        = string
+  default     = "demo-app"
+}
